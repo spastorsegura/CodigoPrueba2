7f1d3e7b3f481c34af07853a49a10d8c1646675a
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-02-07 08:09:11 -0500

    subri

---
+++ semana8/dia1/01-letyconst.js
@@ -0,0 +1,26 @@
+var manzana="apple"
+
+var manzana="manzana acaramelada"
+
+console.log(manzana)
+
+let potato="papa amarilla"
+// let potato="papa morada"
+
+console.log(potato)
+
+for(var i=0;i<3;i++){
+    console.log(i)
+}
+console.log("contador i",i)
+
+for(let j=0;j<3;j++){
+    console.log(j)
+}
+// console.log("contador j",j)
+
+const pi=3.1415;
+
+pi=42
+
+console.log(pi)
---
+++ semana8/dia1/02-classes.js
@@ -0,0 +1,11 @@
+class Gatete{
+    constructor(nombre,edad,raza,color,peso){
+        this.name=nombre
+        this.age=edad
+        this.breed=raza
+        this.colour=color
+        this.weight=peso
+    }
+}
+
+let Gato1 = new Gatete("Abby",7,"angora","blanco",2)
