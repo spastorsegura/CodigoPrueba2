9015cdbf0090a716a62ce1c9fcda1bbf6dbb7751
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-03-12 08:02:43 -0500

    subir

---
+++ JS/prueba.js
@@ -0,0 +1,23 @@
+var nom1="Javier";
+var nom2="Javier";
+var nom3="Javier";
+var nom1="Javier";
+
+
+var almbum_musica = {
+    nombre:"californication",
+    banda:"RHCP",
+    productora:"EMI",
+    anio:1999,
+    canciones:[
+        {
+            titulo:"Scar Tissue",
+            duracion:3.37
+        },
+        {
+            titulo:"Around the world",
+            duracion:3.59 
+        }
+    ]
+}
+
